99159644dce2718ff01e8ccdb0e3eea4a3000942
John L. Mackin <jonnnmackin@gmail.com>
2016-02-22 16:27:46 -0800

    First commit
    Very crude flappy-bird type game. It's a start..

---
+++ app/src/main/java/com/sierra_psec/goadventure/Background.java
@@ -0,0 +1,34 @@
+package com.sierra_psec.goadventure;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+
+/**
+ * Created by Melanos on 2/15/2016.
+ */
+
+public class Background {
+
+    private Bitmap image;
+    private int x, y, dx;
+
+    public Background(Bitmap res){
+        image = res;
+        dx = GamePanel.MOVESPEED;
+    }
+
+    public void update(){
+        x+=dx;
+        if(x<-GamePanel.WIDTH){
+            x=0;
+        }
+    }
+
+    public void draw(Canvas canvas){
+        canvas.drawBitmap(image, x, y, null);
+        if(x<0){
+            canvas.drawBitmap(image,x+GamePanel.WIDTH,y,null);
+        }
+    }
+
+}
---
+++ app/src/main/java/com/sierra_psec/goadventure/GamePanel.java
@@ -0,0 +1,93 @@
+package com.sierra_psec.goadventure;
+
+import android.content.Context;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+/**
+ * Created by Melanos on 2/15/2016.
+ */
+public class GamePanel extends SurfaceView implements SurfaceHolder.Callback{
+
+    public static final int WIDTH = 800;
+    public static final int HEIGHT = 480;
+    public static final int MOVESPEED = -5;
+    private MainThread thread;
+    private Background bg;
+    private Player player;
+
+    public GamePanel(Context context){
+        super(context);
+
+        //add callback to surfaceholder to intercept events
+        getHolder().addCallback(this);
+
+        thread= new MainThread(getHolder(), this);
+
+        //make gamePanel focusable to handle events
+        setFocusable(true);
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height){}
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder){
+        boolean retry = true;
+        while(retry){
+            try{thread.setRunning(false);
+                thread.join();
+            }catch(InterruptedException e){e.printStackTrace();}
+            retry = false;
+        }
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder){
+        bg = new Background(BitmapFactory.decodeResource(getResources(),R.drawable.grassbg1));
+        player = new Player(BitmapFactory.decodeResource(getResources(),R.drawable.helicopter), 65, 25, 3);
+        thread.setRunning(true);
+        thread.start();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+
+        if(event.getAction()==MotionEvent.ACTION_DOWN){
+            if(!player.getPlaying()) {
+                player.setPlaying(true);
+            }
+            player.setUp(true);
+            return true;
+        }
+
+        if(event.getAction()==MotionEvent.ACTION_UP){
+            player.setUp(false);
+            return true;
+        }
+        return super.onTouchEvent(event);
+    }
+
+    public void update(){
+        if(player.getPlaying()){
+            bg.update();
+            player.update();
+        }
+    }
+
+    @Override
+    public void draw(Canvas canvas){
+        final float scaleFactorX = getWidth()/(WIDTH*1.f);
+        final float scaleFactorY = getHeight()/(HEIGHT*1.f);
+        if(canvas!=null) {
+            final int savedState = canvas.save();
+            canvas.scale(scaleFactorX,scaleFactorY);
+            bg.draw(canvas);
+            player.draw(canvas);
+            canvas.restoreToCount(savedState);
+        }
+    }
+}
